d84b4af56799e8c577b3fb404f47d6e46ac6a22c
minjeongHEO <hmjoung9770@gmail.com>
2024-03-07 18:44:32 +0900

    💄: 그리드 부분 css 수정

--- js/index.js
+++ js/index.js
@@ -4,7 +4,8 @@ function navCreate() {
     let newsBrand = document.createElement('div');
     let newsHeadLine = document.createElement('div');
     container.className = 'nav-contents-container';
-    newsHeadLine.className = 'nav-contents-headline'; //margin-left: 29px;
+    newsBrand.className = 'nav-contents-press-name';
+    newsHeadLine.className = 'nav-contents-headline';
     newsBrand.innerHTML = '연합 뉴스';
     newsHeadLine.innerHTML = `'면허정지'에도 꿈쩍않는 전공의…대학들은 "2천명 이상 증...`;
 
@@ -16,33 +17,23 @@ function navCreate() {
 }
 
 function mainNewsCreate() {
-  for (let lineIdx = 0; lineIdx < 4; lineIdx++) {
-    let outerDiv = document.createElement('div');
-    let container = document.createElement('div');
-    container.className = 'row-container';
+  for (let itemIdx = 0; itemIdx < 24; itemIdx++) {
+    let pressListTag = document.createElement('li');
 
-    for (let itemIdx = 0; itemIdx < 6; itemIdx++) {
-      let mediaBox = document.createElement('div');
-      mediaBox.className = 'media-subscription-box';
+    let aTag = document.createElement('a');
+    aTag.href = '#';
+    aTag.className = 'media-subscription-news-view';
 
-      let aTag = document.createElement('a');
-      aTag.href = '#';
-      aTag.className = 'media-subscription-news-view';
-      // aTag.setAttribute('aria-disabled', 'false'); // aria-disabled 속성 설정 ?
+    let imgTag = document.createElement('img');
+    imgTag.src = 'https://s.pstatic.net/static/newsstand/2020/logo/light/0604/057.png';
+    imgTag.height = '20';
+    imgTag.alt = 'MBN';
+    imgTag.className = 'news_logo';
 
-      let imgTag = document.createElement('img');
-      imgTag.src = 'https://s.pstatic.net/static/newsstand/2020/logo/light/0604/057.png';
-      imgTag.height = '20';
-      imgTag.alt = 'MBN';
-      imgTag.className = 'news_logo';
+    aTag.appendChild(imgTag);
 
-      aTag.appendChild(imgTag);
-      mediaBox.appendChild(aTag);
-      container.appendChild(mediaBox);
-    }
-
-    outerDiv.appendChild(container);
-    document.getElementById('main-news-container').appendChild(outerDiv);
+    pressListTag.appendChild(aTag);
+    document.getElementById('press-logo-container').appendChild(pressListTag);
   }
 }
 
