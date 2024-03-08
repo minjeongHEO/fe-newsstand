8cd56b8f43c1cda7f5dcb8577e34664aeef4c125
minjeongHEO <hmjoung9770@gmail.com>
2024-03-08 15:35:09 +0900

    WIP: 헤드라인 데이터 롤링하기

--- js/index.js
+++ js/index.js
@@ -5,20 +5,8 @@ const PAGE = {
   numberOfHeadLineSection: 2,
 };
 
-function headLineCreate() {
-  let navHtml = '';
-  for (let index = 0; index < PAGE.numberOfHeadLineSection; index++) {
-    navHtml += `<div class="nav-contents-container">
-      <div class="nav-contents-press-name">연합 뉴스</div>
-      <div class="nav-contents-headline">'면허정지'에도 꿈쩍않는 전공의…대학들은 "2천명 이상 증...</div>
-    </div>`;
-  }
-  const target = document.getElementById('nav-container');
-  target.innerHTML = navHtml;
-}
-
 async function dataShuffle(jsonArray) {
-  returnjsonArray.sort(() => Math.random() - 0.5);
+  return jsonArray.sort(() => Math.random() - 0.5);
 }
 
 /** 랜덤한 언론사 데이터를 페이지 별로 나누기 */
@@ -39,6 +27,7 @@ async function shuffleAndDivideByPage() {
   return jsonArrPerPage;
 }
 
+/** 페이지 별 화살표 처리 */
 function arrowHandlingByPage() {
   const leftTarget = document.getElementById('angle-left');
   const Righttarget = document.getElementById('angle-right');
@@ -51,6 +40,18 @@ function arrowHandlingByPage() {
     leftTarget.style.visibility = 'visible';
     Righttarget.style.visibility = 'visible';
   }
+}
+
+/**
+ * json데이터 fetch
+ * @param {string} fileName - 'pressData'(언론사), 'headlinesData'(뉴스 헤드라인)
+ */
+async function readJsonFile(fileName) {
+  const filePath = `./${fileName}.json`;
+  const response = await fetch(filePath);
+  const jsonData = await response.json();
+
+  return jsonData; //object (json)
 }
 
 /** */
@@ -71,22 +72,27 @@ async function pressLogoCreate() {
   target.innerHTML = mainNewsHtml;
 }
 
-/** json데이터 fetch */
-async function readJsonFile(fileName) {
-  const filePath = `./${fileName}.json`;
-  const response = await fetch(filePath);
-  const jsonData = await response.json();
-
-  return jsonData; //object (json)
-}
-
+/** 작성 중 */
 async function headlineNewsCreate() {
   const jsonData = await readJsonFile('headlinesData');
   // const obj = JSON.parse(jsonData); //JSON.parse(): JSON 문자열을 JavaScript 객체로 변환(jsonData는 이미 객체임)
+  let headLineHtml = '';
+
+  jsonData.slice(0, jsonData.length / 2);
+  jsonData.slice(jsonData.length / 2);
+
+  for (let index = 0; index < PAGE.numberOfHeadLineSection; index++) {
+    headLineHtml += `<div class="nav-contents-container">
+      <div class="nav-contents-press-name">연합 뉴스</div>
+      <div class="nav-contents-headline">'면허정지'에도 꿈쩍않는 전공의…대학들은 "2천명 이상 증...</div>
+    </div>`;
+  }
+
+  const target = document.getElementById('nav-container');
+  target.innerHTML = headLineHtml;
 }
 
 function pageClick(event) {
-  let page = PAGE.gridPage;
   let target = event.target;
 
   // id가 없을 경우
@@ -130,7 +136,6 @@ function dateCreate() {
 }
 
 dateCreate();
-headLineCreate();
-pressLogoCreate();
 headlineNewsCreate();
+pressLogoCreate();
 clickEvents();
