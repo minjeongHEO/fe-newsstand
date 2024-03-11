972300d655186e1e891cb91ffc67f502b0e8849f
minjeongHEO <hmjoung9770@gmail.com>
2024-03-11 20:47:40 +0900

    WIP: 롤링 애니메이션 구현 중

--- src/js/components/setHeadLineNews.js
+++ src/js/components/setHeadLineNews.js
@@ -2,22 +2,116 @@ import { readJsonFile } from './getJsonFile.js';
 
 const NUMBER_OF_HEADLINE_SECTION = 2;
 
+function createLeftNews(jsonDataLeft) {
+  setInterval(() => {
+    // 'nav-contents-press-name' 클래스를 가진 요소의 하위에서 첫 번째 'ul' 태그
+    let nameUlTag = document.querySelector('.nav-contents-press-name ul');
+    let headlineUlTag = document.querySelector('.nav-contents-headline ul');
+
+    // 새로운 'li' 요소 생성
+    let newNameLi = document.createElement('li');
+    let newHeadlineLi = document.createElement('li');
+
+    // 'li' 요소에 스타일과 내용 설정
+    newNameLi.style.backgroundColor = 'green';
+    newHeadlineLi.style.backgroundColor = 'green';
+    newNameLi.innerHTML = `<a href="${jsonDataLeft[3].newsLink}" target="_blank">33333333${jsonDataLeft[3].newsName}</a>`;
+    newHeadlineLi.innerHTML = `<a href="${jsonDataLeft[3].contentsLink}" target="_blank">33333333${jsonDataLeft[3].contentsHeader}</a>`;
+
+    // 생성한 'li' 요소를 'ul' 요소에 추가
+    nameUlTag.appendChild(newNameLi);
+    headlineUlTag.appendChild(newHeadlineLi);
+  }, 2000);
+}
+
+function removeLeftNews() {
+  setInterval(() => {
+    // 'nav-contents-press-name' 클래스를 가진 요소의 하위에서 첫 번째 'ul' 태그
+    let nameUlTag = document.querySelector('.nav-contents-press-name ul');
+    let headlineUlTag = document.querySelector('.nav-contents-headline ul');
+
+    // 'ul' 태그의 첫 번째 'li' 태그
+    let firstNameLiTag = nameUlTag.querySelector('li');
+    let firstHeadLineLiTag = headlineUlTag.querySelector('li');
+
+    // 있다면 삭제
+    if (firstNameLiTag) nameUlTag.removeChild(firstNameLiTag);
+    if (firstHeadLineLiTag) headlineUlTag.removeChild(firstHeadLineLiTag);
+  }, 2000);
+}
+
+// function removeRightNews(params) {
+//   setInterval(() => {
+//     // 'nav-contents-press-name' 클래스를 가진 요소의 하위에서 첫 번째 'ul' 태그를 찾음
+//     let ulTag = document.querySelector('.nav-contents-press-name ul');
+
+//     // 'ul' 태그의 첫 번째 'li' 태그를 찾음
+//     let firstLiTag = ulTag.querySelector('li');
+
+//     // 'ul' 태그로부터 첫 번째 'li' 태그를 삭제
+//     if (firstLiTag) ulTag.removeChild(firstLiTag);
+//   }, 5000);
+// }
+
 /** 헤드라인 뉴스 생성 */
 export async function setHeadLineNews() {
   const jsonData = await readJsonFile('headlinesData');
-  // const obj = JSON.parse(jsonData); //JSON.parse(): JSON 문자열을 JavaScript 객체로 변환(jsonData는 이미 객체임)
   let headLineHtml = '';
 
-  jsonData.slice(0, jsonData.length / NUMBER_OF_HEADLINE_SECTION);
-  jsonData.slice(jsonData.length / NUMBER_OF_HEADLINE_SECTION);
+  const jsonDataLeft = jsonData.slice(0, jsonData.length / NUMBER_OF_HEADLINE_SECTION);
+  const jsonDataRight = jsonData.slice(jsonData.length / NUMBER_OF_HEADLINE_SECTION);
 
-  for (let index = 0; index < NUMBER_OF_HEADLINE_SECTION; index++) {
-    headLineHtml += `<div class="nav-contents-container">
-      <div class="nav-contents-press-name"><a href="${jsonData[0].newsLink}" target="_blank">${jsonData[0].newsName}</a></div>
-      <div class="nav-contents-headline"><a href="${jsonData[0].contentsLink}" target="_blank">${jsonData[0].contentsHeader}</a></div>
+  // for (let index = 0; index < NUMBER_OF_HEADLINE_SECTION; index++) {
+  headLineHtml += `<div class="nav-contents-container left">
+        <div class="nav-contents-press-name">
+          <ul class="banner_list">
+            <li>
+              <a href="${jsonDataLeft[0].newsLink}" target="_blank">${jsonDataLeft[0].newsName}</a>
+            </li>
+            <li style="background-color: yellow;">
+              <a href="${jsonDataLeft[1].newsLink}" target="_blank">${jsonDataLeft[1].newsName}</a>
+            </li>
+          </ul>
+        </div>
+        <div class="nav-contents-headline">
+          <ul class="banner_list">
+            <li>
+              <a href="${jsonDataLeft[0].contentsLink}" target="_blank">${jsonDataLeft[0].contentsHeader}</a>
+            </li>
+            <li style="background-color: yellow;">
+              <a href="${jsonDataLeft[1].contentsLink}" target="_blank">${jsonDataLeft[1].contentsHeader}</a>
+            </li>
+          </ul>
+        </div>
+      </div>`;
+  // }
+
+  headLineHtml += `<div class="nav-contents-container right">
+      <div class="nav-contents-press-name">
+        <ul class="banner_list">
+          <li>
+            <a href="${jsonDataRight[0].newsLink}" target="_blank">${jsonDataRight[0].newsName}</a>
+          </li>
+          <li>
+            <a href="${jsonDataRight[1].newsLink}" target="_blank">${jsonDataRight[1].newsName}</a>
+          </li>
+        </ul>
+      </div>
+      <div class="nav-contents-headline">
+        <ul class="banner_list">
+          <li>
+            <a href="${jsonDataRight[0].contentsLink}" target="_blank">${jsonDataRight[0].contentsHeader}</a>
+          </li>
+          <li>
+            <a href="${jsonDataRight[1].contentsLink}" target="_blank">${jsonDataRight[1].contentsHeader}</a>
+          </li>
+        </ul>
+      </div>
     </div>`;
-  }
 
   const target = document.getElementById('nav-container');
   target.innerHTML = headLineHtml;
+
+  removeLeftNews();
+  createLeftNews(jsonDataLeft);
 }
