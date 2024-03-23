2226d2c9f45e2edf39fb1fc8dac7e42e996e7fa5
minjeongHEO <hmjoung9770@gmail.com>
2024-03-24 02:46:59 +0900

    feat: 헤드라인 롤링

--- src/js/components/setHeadLineNews.js
+++ src/js/components/setHeadLineNews.js
@@ -1,22 +1,22 @@
 import { readJsonFile } from './getJsonFile.js';
-const DATA = {
-  NUMBER_OF_HEADLINE_SECTION: 2,
-  ROLLING_DATA_COUNT: 2,
-  ROLLING_DATA_IDX: [],
+const ROLLING_DATA = {
+  NUMBER_OF_SECTION: 2,
   INTERVALS: {}, // 인터벌 ID를 저장하기 위한 객체
+  TAG_IDX: [],
+  DIVIDE_JSON: null,
 };
 
 /** 헤드라인 데이터를 섹션 수 별로 나누기 */
-async function divideDataByGrid(jsonData) {
+function divideDataByGrid(jsonData) {
   const jsonDataPerGrid = jsonData.reduce(
     (acc, cur, idx) => {
-      const elementPerGrid = Math.floor(jsonData.length / DATA.NUMBER_OF_HEADLINE_SECTION);
+      const elementPerGrid = Math.floor(jsonData.length / ROLLING_DATA.NUMBER_OF_SECTION);
       // 현재 요소의 인덱스를 사용하여 속할 섹션을 계산
       const sectionIdx = Math.floor(idx / elementPerGrid);
       acc[sectionIdx].push(cur);
       return acc;
     },
-    Array.from({ length: DATA.NUMBER_OF_HEADLINE_SECTION }, () => [])
+    Array.from({ length: ROLLING_DATA.NUMBER_OF_SECTION }, () => [])
   );
   return jsonDataPerGrid;
 }
@@ -27,25 +27,32 @@ const makeHeadLineHTML = (divideJsonData) => {
     acc += `<div class="headline__contents section${idx + 1}">
               <div class="headline__press_name">
                 <div class="headline__rolling_box">
-                  <div class="cur_text"
+                  <div class="pre_text">
                     <a href="${divideJsonData[idx][0].newsLink}" target="_blank">${divideJsonData[idx][0].newsName}</a>
                   </div>
-                  <div class="next_text">
+                  <div class="cur_text">
                     <a href="${divideJsonData[idx][1].newsLink}" target="_blank">${divideJsonData[idx][1].newsName}</a>
+                  </div>
+                  <div class="next_text">
+                    <a href="${divideJsonData[idx][2].newsLink}" target="_blank">${divideJsonData[idx][2].newsName}</a>
                   </div>
                 </div>
               </div>
               <div class="headline__news">
                 <div class="headline__rolling_box">
-                  <div class="cur_text">
+                  <div class="pre_text">
                     <a href="${divideJsonData[idx][0].contentsLink}" target="_blank">${divideJsonData[idx][0].contentsHeader}</a>
                   </div>
-                  <div class="next_text">
+                  <div class="cur_text">
                     <a href="${divideJsonData[idx][1].contentsLink}" target="_blank">${divideJsonData[idx][1].contentsHeader}</a>
+                  </div>
+                  <div class="next_text">
+                    <a href="${divideJsonData[idx][2].contentsLink}" target="_blank">${divideJsonData[idx][2].contentsHeader}</a>
                   </div>
                 </div>
               </div>
             </div>`;
+    ROLLING_DATA.TAG_IDX[idx] = 3;
     return acc;
   }, '');
 
@@ -53,136 +60,91 @@ const makeHeadLineHTML = (divideJsonData) => {
   target.innerHTML = headLineHtml;
 };
 
-/** 헤드라인 태그 html 추가 */
-//appendHeadLineHTML
-function appendHtml(divideJsonData, sectionNum) {
-  if (DATA.ROLLING_DATA_IDX[sectionNum] == undefined) {
-    DATA.ROLLING_DATA_IDX[sectionNum] = DATA.ROLLING_DATA_COUNT;
-  } else {
-    DATA.ROLLING_DATA_IDX[sectionNum]++;
-  }
+const clearAnimationClass = () => {
+  document.querySelectorAll('.pre_text').forEach((e) => e.classList.remove('text_move_up'));
+  document.querySelectorAll('.cur_text').forEach((e) => e.classList.remove('text_move_up'));
+  document.querySelectorAll('.next_text').forEach((e) => e.classList.remove('text_move_up'));
+};
 
-  if (DATA.ROLLING_DATA_IDX[sectionNum] >= divideJsonData[sectionNum].length) {
-    DATA.ROLLING_DATA_IDX[sectionNum] = 0;
-  }
+const addAnimationClass = () => {
+  // QUERY_SELECT_TARGET.ALL_PRE_TEXT.forEach((e) => e.classList.add('text_move_up'));
+  document.querySelectorAll('.pre_text').forEach((e) => e.classList.add('text_move_up'));
+  document.querySelectorAll('.cur_text').forEach((e) => e.classList.add('text_move_up'));
+  document.querySelectorAll('.next_text').forEach((e) => e.classList.add('text_move_up'));
+};
 
-  const pressNameHtml = `<li style="background-color:yellow">
-      <a href="${divideJsonData[sectionNum][DATA.ROLLING_DATA_IDX[sectionNum]].newsLink}" target="_blank">${
-    divideJsonData[sectionNum][DATA.ROLLING_DATA_IDX[sectionNum]].newsName
-  }</a>
-      </li>`;
-
-  const headLineHtml = `<li style="background-color: yellow;">
-                  <a href="${divideJsonData[sectionNum][DATA.ROLLING_DATA_IDX[sectionNum]].contentsLink}" target="_blank">${
-    divideJsonData[sectionNum][DATA.ROLLING_DATA_IDX[sectionNum]].contentsHeader
-  }</a>
-                </li>`;
-
-  const pressNameUlTag = `.nav-contents-container.section${sectionNum + 1} .nav-contents-press-name ul`;
-  const pressNameUlTarget = document.querySelector(pressNameUlTag);
-
-  const headLineUlTag = `.nav-contents-container.section${sectionNum + 1} .nav-contents-headline ul`;
-  const headLineUlTarget = document.querySelector(headLineUlTag);
-
-  pressNameUlTarget.insertAdjacentHTML('beforeend', pressNameHtml);
-  headLineUlTarget.insertAdjacentHTML('beforeend', headLineHtml);
-}
-
-/** 헤드라인 태그 html 삭제 */
-function removeHtml(sectionNum) {
-  const pressNameUlTag = `.nav-contents-container.section${sectionNum + 1} .nav-contents-press-name ul`;
-  const headLineUlTag = `.nav-contents-container.section${sectionNum + 1} .nav-contents-headline ul`;
-  const pressNameUlTarget = document.querySelector(pressNameUlTag);
-  const headLineUlTarget = document.querySelector(headLineUlTag);
-
-  // 'ul' 태그의 첫 번째 'li' 태그
-  let firstNameLiTag = pressNameUlTarget.querySelector('li');
-  let firstHeadLineLiTag = headLineUlTarget.querySelector('li');
-
-  // 있다면 삭제
-  if (firstNameLiTag) pressNameUlTarget.removeChild(firstNameLiTag);
-  if (firstHeadLineLiTag) headLineUlTarget.removeChild(firstHeadLineLiTag);
-}
-
-function applyRollingAnimation(sectionNum) {
-  // 롤링 로직...
-  const ulTag = `.nav-contents-container.section${sectionNum + 1} .banner_list`;
-  const ulTarget = document.querySelectorAll(ulTag);
-  ulTarget.forEach((value) => {
-    value.classList.add('rolling_active');
-
-    setTimeout(() => {
-      value.classList.remove('rolling_active');
-    }, 5000);
+const removeHeadLine = (section) => {
+  document.querySelectorAll(`.section${section + 1} .headline__rolling_box`).forEach((rollingBox) => {
+    // 첫 번째 자식 요소가 존재하는 경우에만 삭제를 시도.
+    // if (rollingBox.firstChild) {
+    if (rollingBox.firstElementChild) {
+      // rollingBox.removeChild(rollingBox.firstChild);
+      rollingBox.removeChild(rollingBox.firstElementChild);
+    }
+    // rollingBox.removeChild(); //이러니까 삭제안됌
+    //removeChild 메소드를 사용할 때는 삭제하고자 하는 자식 노드의 참조를 인자로 전달해야 함.
   });
-}
+};
 
-/** 섹션별로 마우스 오버 시 인터벌 정지 */
-function stopSectionInterval(sectionNum) {
-  if (DATA.INTERVALS[sectionNum]) {
-    clearInterval(DATA.INTERVALS[sectionNum].append);
-    clearInterval(DATA.INTERVALS[sectionNum].remove);
-    clearInterval(DATA.INTERVALS[sectionNum].animate);
-  }
-}
+const addHeadLine = (section) => {
+  const divideJsonData = ROLLING_DATA.DIVIDE_JSON;
+  const pressHTML = `<div class="next_text">
+                        <a href="${divideJsonData[section][ROLLING_DATA.TAG_IDX[section] % 5].newsLink}" target="_blank">
+                          ${divideJsonData[section][ROLLING_DATA.TAG_IDX[section] % 5].newsName}
+                        </a>
+                      </div>`;
+  const pressTarget = document.querySelector(`.section${section + 1} .headline__press_name .headline__rolling_box`);
+  pressTarget.insertAdjacentHTML('beforeend', pressHTML);
 
-function toggleRollingAnimation(sectionNum, rollingPause) {
-  const target = document.querySelector(`.nav-contents-container.section${sectionNum + 1}`);
-  if (rollingPause) {
-    target.classList.add('rolling_pause');
-  } else {
-    target.classList.remove('rolling_pause');
-  }
-}
+  const headlineHTML = `<div class="next_text">
+                          <a href="${divideJsonData[section][ROLLING_DATA.TAG_IDX[section] % 5].contentsLink}" target="_blank">
+                            ${divideJsonData[section][ROLLING_DATA.TAG_IDX[section] % 5].contentsHeader}
+                          </a>
+                        </div>`;
+  const headlineTarget = document.querySelector(`.section${section + 1} .headline__news .headline__rolling_box`);
+  headlineTarget.insertAdjacentHTML('beforeend', headlineHTML);
+  ROLLING_DATA.TAG_IDX[section]++;
+};
 
-/** 마우스 이벤트 리스너 설정 */
-function setupMouseEvents(sectionNum) {
-  const container = document.querySelector(`.nav-contents-container.section${sectionNum + 1}`);
-  if (!container) return;
-
-  container.addEventListener('mouseover', () => {
-    stopSectionInterval(sectionNum); // 마우스 오버 시 인터벌 정지
-    toggleRollingAnimation(sectionNum, true); // 마우스 오버 시 애니메이션 일시정지
+const changeClassName = (section) => {
+  document.querySelectorAll(`.section${section + 1} .headline__rolling_box`).forEach((rollingBox) => {
+    // 첫 번째 div의 클래스 이름을 'pre_text'로 변경
+    if (rollingBox.children[0]) {
+      rollingBox.children[0].className = 'pre_text';
+    }
+    // 두 번째 div의 클래스 이름을 'cur_text'로 변경
+    if (rollingBox.children[1]) {
+      rollingBox.children[1].className = 'cur_text';
+    }
+    // 세 번째 div의 클래스 이름을 'next_text'로 유지 혹은 변경
+    if (rollingBox.children[2]) {
+      rollingBox.children[2].className = 'next_text';
+    }
   });
-
-  container.addEventListener('mouseout', () => {
-    startSectionInterval(sectionNum); // 마우스 아웃 시 인터벌 재시작
-    toggleRollingAnimation(sectionNum, false); // 마우스 아웃 시 애니메이션 재개
-  });
-}
-
-/** 섹션별로 인터벌 시작 */
-function startSectionInterval(divideJsonData, sectionNum) {
-  if (!DATA.INTERVALS[sectionNum]) DATA.INTERVALS[sectionNum] = {};
-
-  // appendHtml 인터벌
-  DATA.INTERVALS[sectionNum].append = setInterval(() => {
-    appendHtml(divideJsonData, sectionNum);
-  }, 5000);
-
-  // removeHtml 인터벌
-  DATA.INTERVALS[sectionNum].remove = setInterval(() => {
-    removeHtml(sectionNum);
-  }, 5000);
-
-  // 롤링 애니메이션 인터벌
-  DATA.INTERVALS[sectionNum].animate = setInterval(() => {
-    applyRollingAnimation(sectionNum);
-  }, 6000 + sectionNum * 1000); // 각 섹션별로 1초의 시간 차
-}
+};
 
 /** 헤드라인 뉴스 생성 */
 export const setHeadLineNews = async () => {
   try {
     const jsonData = await readJsonFile('headlinesData');
-    const divideJsonData = await divideDataByGrid(jsonData);
+    const divideJsonData = divideDataByGrid(jsonData);
+    ROLLING_DATA.DIVIDE_JSON = divideJsonData;
 
     makeHeadLineHTML(divideJsonData);
+    console.log(1);
 
-    // divideJsonData.forEach((element, idx) => {
-    //   startSectionInterval(divideJsonData, idx);
-    //   setupMouseEvents(element,idx);
-    // });
+    setInterval(() => {
+      addAnimationClass();
+      setTimeout(() => {
+        clearAnimationClass();
+        removeHeadLine(0);
+        removeHeadLine(1);
+        addHeadLine(0);
+        addHeadLine(1);
+        changeClassName(0);
+        changeClassName(1);
+      }, 3000);
+    }, 4000);
   } catch (error) {
     console.error(error);
   }
